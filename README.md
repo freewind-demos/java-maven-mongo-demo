fa3a12220257e3dd89358049c1b5e4e3356d1026
Freewind <nowindlee@gmail.com>
2023-07-11 21:06:25 +0800

    java-maven-documentdb-demo

--- README.md
+++ README.md
@@ -1,16 +1,18 @@
-Java Maven Demo
+Java Maven DocumentDB Demo
 =====================
 
-Use maven for Java.
+DocumentDB是AWS提供的与MongoDB兼容的no-sql数据库服务。与MongoDB兼容到直接使用mongodb的命令行和各种sdk就可以访问。
 
-How to run:
+它的问题在于：它创建之后只能在同一个VPC（虚拟私有云）里被其它服务访问，而不允许从外部访问，就算把它的Security Group（防火墙）设置为允许所有流入流量也不行，必须在同一个VPC内创建一台EC2，并在本机电脑使用ssh tunnel将其作为跳板机才行。这相当反直觉，并且配置过程很复杂，对于新人很不友好。
 
-1. Run `Hello.java` in your IDE.
-2. Use maven plugin `exec-maven-plugin`:
-   ```
-   mvn clean compile exec:java
-   ```
-3. Use maven plugin `maven-dependency-plugin` and `maven-jar-plugin`
-   ```
-   mvn clean package; java -jar target/demo.jar
-   ```
+下面这个视频完整展示了整个流程：
+
+- 创建DocumentDB实例
+- 创建同VPC内的EC2实例，并在其上访问DocumentDB
+- 通过SSH隧道将EC2实例配置为跳板机，并从本机访问
+
+视频教程：<https://photos.app.goo.gl/Dk3Kq9AJWkXHAbjz5>
+
+当我们按照教程内容可以在本地访问mongodb后，将其连接字符串复制到Hello.java中，即可试用。
+
+Run `Hello.java` in your IDE.
--- src/main/java/demo/Hello.java
+++ src/main/java/demo/Hello.java
@@ -1,11 +1,38 @@
 package demo;
 
-import org.apache.commons.lang3.StringUtils;
+import com.mongodb.client.*;
+import org.bson.Document;
+
 
 public class Hello {
 
-  public static void main(String[] args) {
-    System.out.println(StringUtils.trim("Hello World!  "));
-  }
+    public static void main(String[] args) {
+        String connectionString = "mongodb://demo:12345678@docdb-2023-07-11-13-15-59.cgg49lhoiw0o.us-east-1.docdb.amazonaws.com:27017/?ssl=true&ssl_ca_certs=global-bundle.gem&retryWrites=false";
+        try (MongoClient client = MongoClients.create(connectionString)) {
+            MongoDatabase db = client.getDatabase("testDb");
+
+            MongoCollection<Document> users = db.getCollection("users");
+
+            long id = System.currentTimeMillis();
+            users.insertOne(createUser(id));
+            users.insertOne(createUser(id + 1));
+            users.insertOne(createUser(id + 2));
+
+            Document query = new Document();
+            query.put("id", id + 1);
+            FindIterable<Document> result = users.find(query);
+            result.forEach(System.out::println);
+        }
+
+    }
+
+    private static Document createUser(long id) {
+        Document doc = new Document();
+        doc.put("id", id);
+        doc.put("name", "aaa" + id);
+        doc.put("email", "aaa" + id + "@test.com");
+        doc.put("age", 111);
+        return doc;
+    }
 
 }
